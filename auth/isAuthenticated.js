aa7bc6ca7968525a11e0c7f2cd761002b9e5446e
MD SAJJAD ANSARI <111681811+sajjad1384N@users.noreply.github.com>
2024-07-31 08:48:32 -1000

    Update isAuthenticated.js

--- auth/isAuthenticated.js
+++ auth/isAuthenticated.js
@@ -4,7 +4,7 @@ const isAuthenticated = async (req,res, next)=>{
         const token = req.cookies.token;
         if(!token){
             return res.status(401).json({
-                message:"User not authenticated",
+                message:"md sajjjad ansari",
                 success:false
             })
         };
