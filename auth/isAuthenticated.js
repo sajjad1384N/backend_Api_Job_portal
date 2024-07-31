dd7ceed3aeceba84eb15a89aaea37830c3cebff4
MD SAJJAD ANSARI <111681811+sajjad1384N@users.noreply.github.com>
2024-07-31 09:06:43 -1000

    Update isAuthenticated.js

--- auth/isAuthenticated.js
+++ auth/isAuthenticated.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 const isAuthenticated = async (req,res, next)=>{
     try {
-        const token = req.cookies.token;
+        const token = req.Cookies.token;
         if(!token){
             return res.status(401).json({
                 message:"User not authenticated",
